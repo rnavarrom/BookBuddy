fe48ab6e242220e35d246927987539ca525f6403
davidp <david_pascualpla@iescarlesvallbona.cat>
2023-03-28 16:56:04 +0200

    Test - Tercer Comit david

--- app/src/main/java/com/example/bookbuddy/MainActivity.kt
+++ app/src/main/java/com/example/bookbuddy/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("Facendo pruebas")
         println("Segundo commit pruebas")
+        println("Tercer commit pruebas")
     }
 }
