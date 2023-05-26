7b162b3a63336a0d52901184c8f92351b2a79c53
rnavarro <rafaelnavarromr@gmail.com>
2023-05-26 16:25:33 +0200

    Update- changes

--- app/src/main/java/com/example/bookbuddy/utils/Constants.kt
+++ app/src/main/java/com/example/bookbuddy/utils/Constants.kt
@@ -5,8 +5,8 @@ import com.example.bookbuddy.R
 
 class Constants {
     companion object{
-        //const val BASE_URL = "https://172.16.24.120:7137/"
-        const val BASE_URL = "https://192.168.1.55:7137/"
+        const val BASE_URL = "https://172.16.24.120:7137/"
+        //const val BASE_URL = "https://192.168.1.55:7137/"
         //const val BASE_URL = "https://192.168.1.53:7137/"
         //const val BASE_URL = "https://172.16.24.198:7137/"
         //const val BASE_URL = "https://172.16.24.136:7137/"
